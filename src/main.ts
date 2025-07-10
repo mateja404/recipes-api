7dd8417ebb340b1b69f4e045db8b13500638249f
mateja404 <markojovanovicc23@outlook.com>
2025-07-10 13:31:44 +0200

    cors fixing

--- src/main.ts
+++ src/main.ts
@@ -11,7 +11,7 @@ async function bootstrap() {
   });
   app.use(helmet());
   app.enableCors({
-    origin: 'http://localhost:3000',
+    origin: '*',
     methods: 'GET, PUT, POST, DELETE, PATCH',
     allowedHeaders: 'Content-Type, Authorization',
   });
