989bece2e43d299cd5c7296cf3ebe24d7913fbf5
mateja404 <markojovanovicc23@outlook.com>
2025-07-10 16:36:31 +0200

    cors credentials issue

--- src/main.ts
+++ src/main.ts
@@ -7,7 +7,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
     origin: '*',
-    credentials: true,
+    credentials: false,
   });
   useContainer(app.select(AppModule), {
     fallbackOnErrors: true
